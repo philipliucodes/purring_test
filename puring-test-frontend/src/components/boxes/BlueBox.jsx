61d70a0633ea2ae386266b4cbfb7e6333eed3b9f
alex tu <tuxuansen@gmail.com>
2025-01-31 23:48:43 -0500

    feat/Finished initial game page

--- puring-test-frontend/src/components/boxes/BlueBox.jsx
+++ puring-test-frontend/src/components/boxes/BlueBox.jsx
@@ -1,13 +1,15 @@
 // components/BlueBox.js
-import React from "react";
+//import React from "react";
 import PropTypes from 'prop-types';
 
 const BlueBox = ({ prompt, buttonText, onGenerate }) => {
   return (
-    <div className="flex flex-col items-center justify-between p-6 w-full rounded-xl bg-blue-600 text-white text-center shadow-xl">
-      <p className="text-md font-semibold overflow-hidden text-ellipsis mb-4 leading-tight">
-        {prompt}
-      </p>
+    <div className="flex flex-col items-center justify-between p-6 w-full aspect-square rounded-xl bg-blue-600 text-white text-center shadow-xl">
+      <div className="flex-grow flex items-center">
+        <p className="text-lg font-semibold overflow-hidden text-ellipsis leading-tight">
+          {prompt}
+        </p>
+      </div>
       <button
         onClick={onGenerate}
         className={`w-full py-3 text-sm font-bold rounded-lg shadow-md uppercase transition-colors text-black ${
